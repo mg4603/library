748c2fc9e7b495b445135480906fde6ee27924e4
Michael George <michael.g.george@gmail.com>
2022-07-26 04:21:09 +0530

    Create classes for book, library and card

--- script.js
+++ script.js
@@ -1,83 +1,122 @@
 const containerDiv = document.querySelector('.container');
 const addBookToLibraryForm = document.getElementById('add-book-form'); 
 
-let myLibrary = [];
-
-function Book(author, title, number_of_pages, readingStatus){
-    this.author = author;
-    this.title = title;
-    this.number_of_pages = number_of_pages;
-    this.readingStatus = readingStatus;
+class Book{
+    #author;
+    #title;
+    #number_of_pages;
+    #readingStatus;
+    constructor(author, title, number_of_pages, readingStatus){
+        super();
+        this.#author = author;
+        this.#title = title;
+        this.#number_of_pages = number_of_pages;
+        this.#readingStatus = readingStatus;
+    }
+    toggleReadStatus(){
+        this.#readingStatus = !this.#readingStatus;
+    }
+    
 }
 
-Book.prototype.toggleReadStatus = function(){
-    this.readingStatus = !this.readingStatus;
+
+
+const myLibrary = class{
+    #books = [];
+    constructor(){
+
+    }
+    addBook(book){
+        this.#books.push(book)
+    }
+    removeBook(book){
+        this.#books = this.#books.filter(bookToCheck=>{
+            return book.title != bookToCheck.title;
+        })
+        //cards have to be re-rendered
+    }
 }
 
-Book.prototype.removeBook = function(){
-    
-    myLibrary = myLibrary.filter(bookToCheck=>{
-        return this.title != bookToCheck.title; 
-    })
-    renderBookCards();
+class Card{
+    #card;
+    #bookTitleH2;
+    #removeBookBtn;
+    #authorP;
+    #ul;
+    #number_of_pagesLi;
+    #readBtnLi;
+    #readToggleBtn;
+
+    constructor(doc){
+        this.#card = doc.createElement('div');
+        this.#bookTitleH2 = doc.createElement('h2');
+        this.#removeBookBtn = doc.createElement('button');
+        this.#authorP = doc.createElement('p');
+        this.#ul = doc.createElement('ul');
+        this.#number_of_pagesLi = doc.createElement('li');
+        this.#readBtnLi = doc.createElement('li');
+        this.#readToggleBtn = doc.createElement('button');
+    }
+
+    //bare
+    #createCard(){
+        this.#removeBookBtn.classList.add('remove-book');
+
+        this.#readBtnLi.appendChild(this.#readToggleBtn);
+        
+        this.#ul.appendChild(this.#number_of_pagesLi);
+        this.#ul.appendChild(this.#readBtnLi);
+
+        this.#card.appendChild(this.#bookTitleH2);
+        this.#card.appendChild(this.#removeBookBtn);
+        this.#card.appendChild(this.#authorP);
+        this.#card.appendChild(this.#ul);
+    }
+
+    #readingStatusToText(readingStatus){
+        return readingStatus == 1? "Read": "Unread";
+    }
+
+    #populateCard(book){
+        this.#card.classList.add('card');
+        this.#bookTitleH2.textContent = book.title;
+        this.#authorP.textContent = book.author;
+        this.#number_of_pagesLi.textContent = book.number_of_pages;
+        this.#readToggleBtn.textContent = this.#readingStatusToText(book.readingStatus);
+    }
+
+    createBookCard(book){
+        this.#createCard();
+        this.#populateCard(book);
+    }
+
 }
 
-function displayBook(book){
-    const card = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const removeBook = document.createElement('button');
-    const p = document.createElement('p');
-    const ul = document.createElement('ul');
-    const number_of_pages = document.createElement('li');
-    const readLi = document.createElement('li');
-    const readToggle = document.createElement('button');
-    
-
-    removeBook.classList.add('remove-book');
-    // removeBook.textContent = "x";
-
-    card.classList.add('card');
-    h2.textContent = book.title;
-    p.textContent = book.author;
-    number_of_pages.textContent = book.number_of_pages;
-    readToggle.textContent = book.readingStatus == 1?"Read":"Unread";   
-    
-    removeBook.addEventListener('click', ()=>{
+const DisplayController = (function(doc){
+    this.#removeBookBtn.addEventListener('click', ()=>{
         book.removeBook();
     });
-    
-    readToggle.addEventListener('click', ()=>{
+    this.#readToggleBtn.addEventListener('click', ()=>{
         book.toggleReadStatus();
-        readToggle.textContent = book.readingStatus == 1?"Read":"Unread";   
+        this.#readToggleBtn.textContent = this.#readingStatusToText(1);
     });
 
+    function _deleteAllChildrean(node){
+        while(node.hasChildNodes()){
+            node.removeChild(node.lastChild);
+        }
+    }
 
-    readLi.appendChild(readToggle);
-    ul.appendChild(number_of_pages);
-    ul.appendChild(readLi);
 
-    card.appendChild(h2);
-    card.appendChild(removeBook);
-    card.appendChild(p);
-    card.appendChild(ul);
+})(document);
 
-    return card;
-}
 
 function renderBookCards(){
     deleteAllChildren(containerDiv);
     myLibrary.forEach(book=>{containerDiv.appendChild(displayBook(book))})
 }
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
-}
 
-function deleteAllChildren(node){
-    while(node.hasChildNodes()){
-        node.removeChild(node.lastChild);
-    }
-}
 
 const addBookBtn = document.getElementById('add-book');
 addBookBtn.addEventListener('click', ()=>{
